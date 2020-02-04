2f61468dad0e50e8ce9571ba2f3c0373fbdf2b76
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-04 02:15:14 -0300

    practicando

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,7 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
-{
-	alert("Esto funciona de maravilla");
+{// enseña alerta visual con texto
+
+	alert("vamooo que va saliendo lol");
 }
 
