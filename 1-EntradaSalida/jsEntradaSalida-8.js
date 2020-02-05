dbb38f37fc1cf8ee1c52e28d519f65289d84daab
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-05 02:31:10 -0300

    resto %

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -3,6 +3,13 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
-{
+{// simbolo % = modulo o resto
+    var numerouno;
+    var numerodos;
+    var resultado;
+    numerouno = parseInt(document.getElementById("numeroDividendo").value);
+    numerodos = parseInt(document.getElementById("numeroDivisor").value);
+    resultado = numerouno%numerodos;
+    alert("El resto es: "+resultado);
 	
 }
