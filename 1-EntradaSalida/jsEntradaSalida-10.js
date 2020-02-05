57ea5686ceb759ed6fdc1452771cfe1e65874d35
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-05 03:28:57 -0300

    similar al 9

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,6 +3,11 @@ Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
-{
-	
+{var importe;
+    var descuento;
+    var sueldodescuento;
+importe = parseInt(document.getElementById("importe").value);
+descuento = (importe *25)/100;
+sueldodescuento = importe - descuento;
+document.getElementById("resultado").value = sueldodescuento;	
 }
