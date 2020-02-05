e22a2a8c9ad3384857d05e7a78b10bb4a7807f6b
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-05 01:38:14 -0300

    mas estructurado

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,7 +2,18 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
-{
+{//establesco variables(desarmé el problem en partes)
+    var numerouno;
+    var numerodos;
+    var resultado;
+    //asigno funcionalidad
+    numerouno = parseInt(document.getElementById("numeroUno").value);
+    numerodos = parseInt(document.getElementById("numeroDos").value);
+    resultado = numerouno+numerodos;
+    //muestro asignacion
+    alert("La suma es: "+resultado);
+    //tengo 2 numeros, 1 operacion, 1 resultado que mostrar(3 variables 2 asignaciones 1 resultado que mostrar
+
 
 }
 
