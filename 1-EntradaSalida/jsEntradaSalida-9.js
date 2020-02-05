6a90173958debc1719b15ac8c149a6a648fd7166
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-05 03:10:14 -0300

    hecho,falta pensarlo mas

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -3,7 +3,16 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
-{
-
-	
+{//establesco variables
+    var sueldo;
+    var aumento;
+    var sueldoaumentado;
+//capturo lo que esta adentro de la casilla sueldo(asignacion)    
+sueldo = parseInt(document.getElementById("sueldo").value);
+//operacion de porcentaje
+aumento = (sueldo *10)/100;
+//hago la operacion final
+sueldoaumentado = aumento + sueldo;
+// capturo la casilla para mostrar la asignacion de la variable
+document.getElementById("resultado").value = sueldoaumentado;	
 }
