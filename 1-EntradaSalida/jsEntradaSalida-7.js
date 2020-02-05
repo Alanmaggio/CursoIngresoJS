8351f90df6ef427e675fcec91fa40ee9aa5b97dc
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-05 01:51:33 -0300

    casi igual al 6

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,22 +3,46 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{	var numerouno;
+    var numerodos;
+    var resultado;
+    numerouno = parseInt(document.getElementById("numeroUno").value);
+    numerodos = parseInt(document.getElementById("numeroDos").value);
+    resultado = numerouno+numerodos;
+    alert("La suma es: "+resultado);
 	
 }
 
 function restar()
-{
+{var numerouno;
+    var numerodos;
+    var resultado;
+    numerouno = parseInt(document.getElementById("numeroUno").value);
+    numerodos = parseInt(document.getElementById("numeroDos").value);
+    resultado = numerouno-numerodos;
+    alert("La resta es: "+resultado);
 	
 }
 
 function multiplicar()
-{ 
+{ var numerouno;
+    var numerodos;
+    var resultado;
+    numerouno = parseInt(document.getElementById("numeroUno").value);
+    numerodos = parseInt(document.getElementById("numeroDos").value);
+    resultado = numerouno*numerodos;
+    alert("La multiplicación es: "+resultado);
 	
 }
 
 function dividir()
-{
+{var numerouno;
+    var numerodos;
+    var resultado;
+    numerouno = parseInt(document.getElementById("numeroUno").value);
+    numerodos = parseInt(document.getElementById("numeroDos").value);
+    resultado = numerouno/numerodos;
+    alert("La división es: "+resultado);
 	
 }
 
