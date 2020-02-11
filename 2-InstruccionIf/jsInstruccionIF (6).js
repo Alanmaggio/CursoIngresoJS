0c04ab082151a2f6e32085d85d9b677dd9189010
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-11 03:42:08 -0300

    en casa

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,13 @@
 function mostrar()
 {
 //tomo la edad  
-
-
-
-
+var edad;
+ edad = parseInt(document.getElementById("edad").value);
+if (edad <=17 && edad >= 13){
+    alert("es adolescente");
+}else if (edad <13){
+    alert("es un niño");
+}else if (edad >18){
+    alert("Es adulto");
+}
 }//FIN DE LA FUNCIÓN
