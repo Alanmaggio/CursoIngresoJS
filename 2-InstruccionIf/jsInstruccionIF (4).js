15e938e8491a47ca94a1772b388e1899fe1b3d0c
alumno <alumno@utnfra.com.ar>
2020-02-10 22:22:54 -0300

    en clase

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,17 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+edad = parseInt(document.getElementById("edad").value);
 
 
+// no esta detro del rango
+if (!(edad <13 || edad >17 )){
+alert("Es adolescente");
+}
+//    esta adentro del rango?
+/*if (edad >= 13 && edad <=17){
+    alert("es adolescente");
+}
 
 }//FIN DE LA FUNCIÓN
