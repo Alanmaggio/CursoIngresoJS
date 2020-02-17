b642b04472886db1a8ec8809e6354e2b2956c27b
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-17 02:50:42 -0300

    en casa/no se si esta bien

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -5,14 +5,26 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 */
 
 function Rectangulo () 
-{
+{var largo = parseFloat(document.getElementById("Largo").value);
+var ancho = parseFloat(document.getElementById("Ancho").value);
+var operacion = ((largo + ancho)*3);
+alert("La cantidad es "+operacion);
 
 }
 function Circulo () 
-{
-	
+{var radio = parseFloat(document.getElementById("Radio").value);
+var operacion = (radio * 3);
+alert("La cantidad es "+operacion);	
 }
 function Materiales () 
-{
+{var largo = parseFloat(document.getElementById("Largo").value);
+var ancho = parseFloat(document.getElementById("Ancho").value);
+var area = largo * ancho;
+var cemento = area*2;
+var cal = area*3;
+alert(" Se necesitan "+cemento+" bolsas de cemento y "+cal+" bolsas de cal")
+
+
+
 	
 }
