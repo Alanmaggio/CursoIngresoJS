19be8bb5e15c064cd62d26efdeafe11d7cbea4ac
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-16 22:46:15 -0300

    en casa/ref en internet

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,8 +1,12 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	var aleatorio;
-	aleatorio = Math.round(Math.random()*10);
-	alert ("tu numero es "+aleatorio);	
-
+	var aleatorio = parseInt(Math.random()*10)+1;
+	if (aleatorio >=9){
+		alert("Exelente");
+	}
+else if (aleatorio <=8 && aleatorio >=4){
+	alert("Aprobo");
+}	
+else alert("Vamos, la proxima se puede");
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,9 +2,11 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	var aleatorio;
-	aleatorio = parseInt(Math.random()*11);
-	if (aleatorio == 0){
+	aleatorio = parseInt(Math.random()*10)+1;
 	alert ("tu numero es "+aleatorio);
-}	
+
+
+
+
+	/*alert(parseInt(Math.random(10 - 1)*10));*/
 }//FIN DE LA FUNCIÓN
-//hay q terminarlo
