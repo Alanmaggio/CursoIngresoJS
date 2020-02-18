8b983d71c72feb1297126fc00ae3669e133b79c8
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-18 02:04:48 -0300

    HnK

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,8 +2,29 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch (mesDelAño)
+{
+case (mesDelAño = "Enero"):;
+    case (mesDelAño = "Febrero"):
+    case (mesDelAño = "Marzo"):
+    case (mesDelAño = "Abril"):
+    case (mesDelAño = "Mayo"):
+    alert("Falta para el invierno.");
+    break;
+    case (mesDelAño = "Junio"):
+    case (mesDelAño = "Julio"):
+    case (mesDelAño = "Agosto"):
+    alert("Abrigate que hace frio.");
+    break;
+    case (mesDelAño = "Septiembre"):
+    case (mesDelAño = "Octubre"):;
+    case (mesDelAño = "Noviembre"):
+    case (mesDelAño = "Diciembre"):
+    alert("Ya paamos el frio, ahora calo!!!..");
+    break;
+}
 
-alert (mesDelAño);
+//alert (mesDelAño);
 
 
 
