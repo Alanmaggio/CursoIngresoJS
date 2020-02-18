b4f80537d3957a9680f09d3ce386c557b6504163
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-18 02:59:47 -0300

    HnK

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,7 +1,17 @@
 function mostrar()
 {
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
+var laHora = parseInt(document.getElementById('hora').value);
+switch (laHora)
+{
+case (laHora = 7):
+case (laHora = 8):
+case (laHora = 9):
+case (laHora = 10):
+case (laHora = 11):
+        alert("Es de mañana.");
+        break;    
+}
 
 //alert (laHora);
 	
