61320edc6e4830a56b9fb6ebf1381875d2010384
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-25 03:53:43 -0300

    HnK

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,9 @@
 function mostrar()
-{
-	alert('iteración while');
-
-
+{var num = 11;
+	//mientras num sea menorigual a 11 y menorigual a 1
+	while (num <=11 && num >=1) {
+		//hacer num menos 1
+		num = num - 1;
+	alert(''+num);
+}
 }//FIN DE LA FUNCIÓN
