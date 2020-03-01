800d33eb8786e8c17edfe37159085bc0b484dd49
Alanmaggio <Alanmaggio221991@gmail.com>
2020-02-29 23:54:31 -0300

    HnK

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,8 +3,5 @@ function mostrar()
 while (clave != "utn750"){
     var clave = prompt("ingrese el número clave.");
 }
-if (clave == "utn750"){
     alert("Tu clave es valida");
-}
-
 }//FIN DE LA FUNCIÓN
