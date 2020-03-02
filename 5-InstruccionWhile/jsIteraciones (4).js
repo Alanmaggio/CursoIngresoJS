ade0caec42f8f6c841098c087bd47ec9ae1db6db
Alanmaggio <Alanmaggio221991@gmail.com>
2020-03-02 03:09:52 -0300

    commit

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,9 +2,9 @@ function mostrar()
 {
 	var numero = prompt("ingrese un número entre 0 y 10.");
 	
-	while (numero >=10)
+	while (numero >=10 || numero <0)
 	{
-		alert("tu numero no es correcto");
+		alert("tu numero no es valido");
 		numero = prompt("ingrese un número entre 0 y 10.");
 	}	
 	alert("Tu numero es valido");
