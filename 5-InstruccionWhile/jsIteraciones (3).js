8451a48bb2db39608ad943dd989f5c0506cb896f
Alanmaggio <Alanmaggio221991@gmail.com>
2020-03-02 02:48:26 -0300

    commit

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,10 @@
 function mostrar()
 {
-while (clave != "utn750"){
-    var clave = prompt("ingrese el número clave.");
+var clave = prompt("ingrese el número clave.");
+    while (clave != "utn750")
+{
+    alert("tu clave no es valida. Intentalo nuevamente")
+    clave = prompt("ingrese el número clave.");
 }
     alert("Tu clave es valida");
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,11 @@
 function mostrar()
 {
-
 	var numero = prompt("ingrese un número entre 0 y 10.");
-
-
+	
+	while (numero >=10)
+	{
+		alert("tu numero no es correcto");
+		numero = prompt("ingrese un número entre 0 y 10.");
+	}	
+	alert("Tu numero es valido");
 }//FIN DE LA FUNCIÓN
