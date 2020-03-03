a70637010b0d742c21f148fdd06a7eb9acf00c83
alumno <alumno>
2020-03-02 21:41:49 -0300

    Reps

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,12 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var repeticiones = parseInt(prompt("ingrese el número de repeticiones"));
 
+for(var i = 0 ;i <10  ; i++)
+{
+    document.write("Hola UTN-FRA");
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,24 @@
 function mostrar()
 {
+    var impares=0;
+    var pares=0;
+    var ingreso = parseInt(prompt("Ingrese un número"));
 
+    // Para orden descendente: 
+    for(var i= ingreso; i>0; i--){
+    //Para orden ascendente:
+    //for (var i = 1; ingreso+1>i; i++) {
+        
+        while (isNaN(ingreso)) {
+            ingreso = parseInt(prompt("Reingrese un número"));
+        }
+        
+        if (i%2==0) {
+            console.log(i);
+            pares++;
+        }
+     }
+     alert("Cantidad de números pares: "+pares);  
 
-
-
+        
 }//FIN DE LA FUNCIÓN
