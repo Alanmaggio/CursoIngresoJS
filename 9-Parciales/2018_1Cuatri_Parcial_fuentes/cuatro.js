0e2591197258de57b3452681d9c67cad6b65c2fa
unknown <Alanmaggio221991@gmail.com>
2020-03-06 02:22:18 -0300

    DnHouse

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,22 @@
-function mostrar()
-{
+function mostrar() {
+    var numUno = parseInt(prompt("Ingrese el primer numero"));
+    var numDos = parseInt(prompt("Ingrese el segundo numero"));
+    var operacionResta = numUno - numDos;
+    var operacionsuma = numUno + numDos;
+    var resultado;
 
+    if (numUno == numDos) {
+        alert("los numeros son iguales :" + numUno + numDos);
+
+    } else if (numUno > numDos) {
+        alert("los numeros son iguales :" + operacionResta);
+        resultado = operacionResta;
+    }
+    else if (numUno < numDos) {
+        alert("los numeros son iguales :" + operacionsuma);
+        resultado = operacionsuma;
+    }
+}
+if (resultado > 10) {
+    alert("el resultado es : " + resultado);
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,9 @@
 
 function mostrar()
 {
+    var largo = parseInt(prompt("Ingrese ancho"));
+var ancho = parseInt(prompt("Ingrese el largo"));
+var operacion = ancho*2+largo*2;
+alert("El perimetro es :"+operacion);
 
 }
