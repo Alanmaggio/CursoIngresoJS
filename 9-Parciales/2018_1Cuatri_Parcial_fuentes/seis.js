27ca227ce89a2924966e9d792077b79f0aed7208
Alanmaggio <Alanmaggio221991@gmail.com>
2020-03-11 05:09:45 -0300

    HnK

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,7 +1,32 @@
 function mostrar() {
     var hora = parseInt(document.getElementById("laHora").value);
-
-    if (hora == null) {
-        alert("Ingrese una hora valida");
-    }
-}
+    switch (hora) {
+        case (hora = "6"):
+        case (hora = "7"):
+        case (hora = "8"):
+        case (hora = "9"):
+        case (hora = "10"):
+        case (hora = "11"):
+            alert("Es de mañana.");
+            break;
+        case (hora = "12"):
+        case (hora = "13"):
+        case (hora = "14"):
+        case (hora = "15"):
+        case (hora = "16"):
+        case (hora = "17"):
+        case (hora = "18"):
+        case (hora = "19"):
+            alert("Es de tarde..");
+            break;
+        case (hora = "20"):
+        case (hora = "21"):
+            alert("Es de noche.");
+            break;
+        case (hora = "22"):
+        case (hora = "23"):
+        case (hora = "24"):
+            alert("A dormir!!!");
+            break;
+} 
+}          
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,25 @@
-function mostrar()
-{
+function mostrar() {
 
+    var contNotasM = 0;
+    var contNotas = 0;
+    var nota;
+    var sexo;
+
+
+
+
+    do
+    {  (isNaN(nota) || nota == null || nota <=0 || nota >10) 
+        nota = parseInt(prompt("Ingrese la nota"));
+        alert("Ingrese una nota valida.");
+    }
+    while (nota >= 1 && nota <=10){ 
+        nota = parseInt(prompt("Ingrese la nota"));
+        contNotas ++;
+}
+/*while (sexo == null || sexo != "f" || sexo != "m") {
+    alert("Ingrese sexo valido");
+}
+
+*/
 }
