45fec2a27e7a0e0849e30d3d2ea1d2251cbf627d
Alanmaggio <Alanmaggio221991@gmail.com>
2020-03-13 08:57:26 -0300

    HnK

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,21 @@
-function mostrar()
-{
+function mostrar() {
+	var numero;
+	var contador = 0;
+	var acumulador = 0;
+	var respuesta = 'si';
 
-	var contador=0;
-	var acumulador=0;
-	var respuesta='si';
 
+	while (respuesta == "si") {
+		numero = parseInt(prompt("Ingrese numero"));
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+	while (isNaN(numero)) {
+		numero = alert("Escribe un numero valido");
+
+	}	contador++
+		acumulador = acumulador + numero;
+		respuesta = prompt("Desea continuar?");
+	}
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = acumulador / contador;
 
 }//FIN DE LA FUNCIÓN
