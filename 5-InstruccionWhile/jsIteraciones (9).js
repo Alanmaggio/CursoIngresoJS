22fc27e3368742d15dc305c49b96eb5f0b0c0a67
Alanmaggio <Alanmaggio221991@gmail.com>
2020-03-15 05:49:04 -0300

    HnK

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,43 @@
-function mostrar()
-{
-
-	var contador=0;
+function mostrar() {
 	// declarar variables
-	
-	var respuesta='si';
+	var numero;
+	var contador = 0;
+	var respuesta = 'si';
+	var maximo;
+	var minimo;
 
-	while(respuesta!='no')
-	{
-		
-	
+	while (respuesta != 'no') {
+		numero = parseInt(prompt("Ingrese numero"));
+		while (isNaN(numero) || null) {
+			numero = prompt("Escribe un numero valido");
+		}
+		if (contador == 0) {
+			maximo = numero;
+			minimo = numero;
+			contador = 1;
+		} else if (numero > maximo) {
+			maximo = numero;
+		} else if (numero < minimo) {
+			minimo = numero;
+		}
+		respuesta = prompt("Desea continuar?");
 	}
-
-
-
+	document.getElementById("maximo").value = maximo;
+	document.getElementById("minimo").value = minimo;
 
 }//FIN DE LA FUNCIÓN
+
+/* MAXIMO Y MINIMO UTILISANDO UN IF CON UN FLAG.
+ EN MI CASO LO DEJE CON EL NOMBRE "CONTADOR" PARA IGUAL
+  ACORDARME QUE UN FLAG ES ESO.
+
+  var flag = 0;
+if (flag == 0) {
+			maximo = numero;
+			minimo = numero;
+			flag = 1;
+		} else if (numero > maximo) {
+			maximo = numero;
+		} else if (numero < minimo) {
+			minimo = numero;
+		}*/
